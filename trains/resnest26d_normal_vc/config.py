615ca8cda281b852cb0c2b8b10ba381f322bfe20
AiEson <2368442877@qq.com>
2023-01-05 18:02:06 +0800

    update

--- trains/resnest26d_normal_vc/config.py
+++ trains/resnest26d_normal_vc/config.py
@@ -4,8 +4,8 @@ import sys
 import inspect
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), '../../')))
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), './')))
-from models.resnest_mvc_if import ResNeStMVCUnetPPModel
+from models.resnest_normal_vc_if import ResNeStNormalVCUnetPPModel
 
 N_NAME = os.path.basename(os.path.dirname(inspect.getfile(inspect.currentframe())))
 
-MODEL_INTERFACE = ResNeStMVCUnetPPModel
+MODEL_INTERFACE = ResNeStNormalVCUnetPPModel
